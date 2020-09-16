9c98f3876ee1279a89df11a8b263a93fde4e174d
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-09-16 23:56:13 +0300

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.io.Serializable;
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 5;
+          String s = "Hello";
+          String space = " ";
+          String name = "Bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello" + " " + "John");
+        System.out.println("My number is " + x);
+    }
+}
